af05514da8ef890e0cc2c0be75338c11aee65ac9
Saad Malik <saadmalik806@gmail.com>
2026-05-10 20:37:30 +0530

    Initial commit: Angular project setup

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,36 @@
+import { Component, input, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
+
+@Component({
+  selector: 'app-tasks',
+  imports: [TaskComponent, NewTaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+  @Input({required: true}) userid!: string;
+  @Input({required: true}) name!: string;
+  isAddingTask = false;
+  
+  constructor(private tasksService: TasksService) {}
+
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.userid);
+  }
+
+  // onCompleteTask(id: string){
+  //   this.tasksService.removeTask(id);
+  // }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+  }
+
+  onCloseAddTask(){
+    this.isAddingTask = false;
+  }
+
+}
